15109c5f001e0333afa5d3f34f02af3eb3891b67
e1177 <e1177@axbom.se>
2017-04-25 09:58:14 +0200

    Update e1177.js

--- e1177.js
+++ e1177.js
@@ -346,7 +346,7 @@ for (i = dateCounter; i < 800; i++) {
 }
 
 if (noDays) {
-	dateMenu += '<option value="zero">Det finns inga lediga tider utlagda under perioden.</option>';
+	dateMenu += '<option value="zero">Det finns inga lediga tider utlagda under perioden</option>';
 }
 
 dateMenu += '<option value="one">Sök längre fram i tiden</option>';
